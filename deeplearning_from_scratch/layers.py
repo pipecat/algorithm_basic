1601b8a79ec7dee92a15b52893b8b758822165ca
zhaorunyuan <zhaorunyuan@ebupt.com>
2021-03-17 23:52:27 +0800

    add layers

--- deeplearning_from_scratch/layers.py
+++ deeplearning_from_scratch/layers.py
@@ -36,3 +36,57 @@ class MulLayer:
         dy = dout * self.x
 
         return dx, dy
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = ( x <= 0 )
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        self.y = 1 / (1 + np.exp(-x))
+        out = self.y
+
+        return out
+    
+    def backward(self, dy):
+        dx = dy * self.y * (1.0 - self.y)
+        
+        return dx
+
+class Affine:
+    
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        self.dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
